a444753bd751096084e261b6a360a075e9e1b549
Guillaume Lagrange <guillaume.lagrange@telecom-paristech.fr>
2017-03-28 23:24:23 +0200

    Debuging WIP

--- jumping_monkey/main.cpp
+++ jumping_monkey/main.cpp
@@ -8,6 +8,7 @@
 #include <cstdint>
 #include <queue>
 #include <utility>
+#include <unordered_set>
 
 #define MAXN 21
 
@@ -16,55 +17,59 @@ using namespace std;
 int n, m;
 uint32_t adj[MAXN];
 uint32_t curr_state; // Contains wether the monkey can be at each tree or not
-vector<uint32_t> state_history; // Register already visited states
+unordered_set<uint32_t> state_history; // Register already visited states
 queue<pair<int, pair<uint32_t, vector<int>>>> bfs_queue; // Queue used for BFS
 
 /* Generates the new state if node shot is shot at */
 uint32_t new_state(int shot) {
-    state_history.push_back(curr_state);
     curr_state &= ~(1<<shot);
-    uint32_t new_state = 0;
+    uint32_t n_state = 0;
+
     for(int i=0; i<n; i++) {
         if(curr_state & (1<<i))
             for(int j=0; j<n; j++)
                 if(adj[i] & (1<<j))
-                    new_state |= (1<<j);
-    } // TODO : tester si on a déjà vu l'état ou non, intégrer ça à la BFS
-    return new_state;
+                    n_state |= (1<<j);
+    }
+    return n_state;
 }
 
 void bfs()
 {
-    /* Empty queue */
+    /* Empty queue and clear state_history */
     while(!bfs_queue.empty())
         bfs_queue.pop();
+    state_history.clear();
+    state_history.insert((1<<n) - 1);
 
     /* Fill queue with n first states */
     for(int i=0; i<n; i++)
-        bfs_queue.push(make_pair(i, make_pair(0, vector<int>())));
+        bfs_queue.push(make_pair(i, make_pair((1<<n) - 1, vector<int>(0))));
 
     /* Start of BFS */
     while(!bfs_queue.empty()) {
         auto tmp = bfs_queue.front();
-        bfs_queue.pop();
         int i = tmp.first;
         curr_state = tmp.second.first;
 
+        /* If current state is ok, we leave it at the front fo queue for main */
         if(curr_state == 0)
-            break;
+            return;
 
+        bfs_queue.pop();
         int n_state = new_state(i);
 
         /* If new state has never been seen before */
         if(find(state_history.begin(), state_history.end(), n_state)
                 == state_history.end()) {
-            state_history.push_back(n_state);
+            state_history.insert(n_state);
             /* Adding all potential shots to the queue */
             vector<int> curr_path = tmp.second.second;
             for(int j=0; j<n; j++)
-                if(n_state & (1<<j)) {
+                if(n_state & (1<<j) || n_state == 0) {
                     /* Copying old path */
                     vector<int> n_path = vector<int>(curr_path);
+                    n_path.push_back(j);
                     bfs_queue.push(make_pair(j, make_pair(n_state, n_path)));
                 }
         }
@@ -74,7 +79,8 @@ void bfs()
 
 int main() {
     while(true) {
-        scanf("%d %d", &n, &m);
+        if(scanf("%d %d", &n, &m) != 2)
+            return -1;
 
         if(n==0 && m==0)
             break;
@@ -82,7 +88,8 @@ int main() {
         /* Registering adjencies */
         for(int i=0; i<m; i++) {
             int a,b;
-            scanf("%d %d", &a, &b);
+            if (scanf("%d %d", &a, &b) != 2)
+                return -1;
             adj[a] |= (1<<b);
             adj[b] |= (1<<a);
         }
@@ -95,7 +102,16 @@ int main() {
 
         bfs();
 
-
+        if(bfs_queue.empty()) {
+            puts("Impossible");
+        } else {
+            auto tmp = bfs_queue.front();
+            auto shots = tmp.second.second;
+            printf("%d :", (int) shots.size());
+            for(auto tree : shots)
+                printf(" %d", tree);
+            printf("\n");
+        }
     }
 
     return 0;
