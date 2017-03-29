899c9a3648123108c7e55aeb4afde2a483755303
Guillaume Lagrange <guillaume.lagrange@telecom-paristech.fr>
2017-03-29 16:07:54 +0200

    Accepted

--- jumping_monkey/main.cpp
+++ jumping_monkey/main.cpp
@@ -18,8 +18,7 @@ using namespace std;
 int n, m;
 uint32_t adj[MAXN];
 uint32_t curr_state; // Contains wether the monkey can be at each tree or not
-unordered_set<uint32_t> state_history; // Register already visited states
-queue<pair<int, pair<uint32_t, vector<int>>>> bfs_queue; // Queue used for BFS
+vector<int> success_path;
 
 /* Generates the new state if node shot is shot at */
 uint32_t new_state(int shot) {
@@ -35,11 +34,10 @@ uint32_t new_state(int shot) {
 
 void bfs()
 {
-    /* Empty queue and clear state_history */
-    while(!bfs_queue.empty())
-        bfs_queue.pop();
-    state_history.clear();
-    state_history.insert((1<<n) - 1);
+    unordered_set<uint32_t> state_history; // Register already visited states
+    queue<pair<int, pair<uint32_t, vector<int>>>> bfs_queue; // Queue used for BFS
+
+    success_path.clear();
 
     /* Fill queue with n first states */
     for(int i=0; i<n; i++)
@@ -48,27 +46,28 @@ void bfs()
     /* Start of BFS */
     while(!bfs_queue.empty()) {
         auto tmp = bfs_queue.front();
+        bfs_queue.pop();
         int i = tmp.first;
         curr_state = tmp.second.first;
-
-        /* If current state is ok, we leave it at the front fo queue for main */
-        if(curr_state == 0)
-            return;
-
-        bfs_queue.pop();
         int n_state = new_state(i);
+
+        /* Handling if we got the monkey */
+        if(n_state == 0) {
+            success_path = tmp.second.second;
+            success_path.push_back(i);
+            break;
+        }
 
         /* If new state has never been seen before */
         if(find(state_history.begin(), state_history.end(), n_state)
                 == state_history.end()) {
             state_history.insert(n_state);
             /* Adding all potential shots to the queue */
-            vector<int> curr_path = tmp.second.second;
+            vector<int> n_path = tmp.second.second;
+            n_path.push_back(i);
             for(int j=0; j<n; j++)
-                if(n_state & (1<<j) || n_state == 0) {
+                if(n_state & (1<<j)) {
                     /* Copying old path */
-                    vector<int> n_path = vector<int>(curr_path);
-                    n_path.push_back(i);
                     bfs_queue.push(make_pair(j, make_pair(n_state, n_path)));
                 }
         }
@@ -103,13 +102,11 @@ int main() {
 
         bfs();
 
-        if(bfs_queue.empty()) {
+        if(success_path.empty()) {
             puts("Impossible");
         } else {
-            auto tmp = bfs_queue.front();
-            auto shots = tmp.second.second;
-            printf("%d :", (int) shots.size());
-            for(auto tree : shots)
+            printf("%d:", (int) success_path.size());
+            for(auto tree : success_path)
                 printf(" %d", tree);
             printf("\n");
         }
